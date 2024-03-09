4c56f9cecf3347508a04c1ed16da60f79a30549d
Amanda Natallie <amanda.natallie.2@gmail.com>
2024-03-08 22:13:56 -0300

    chore(architecture): add playright for e2e tests

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,18 +1,14 @@
 'use client';
 import Link from 'next/link';
 import React, { useRef } from 'react';
-import Webcam from 'react-webcam';
 
 type Props = {};
 
 const HomePage = (props: Props) => {
-  const webCamRef = useRef<Webcam>(null);
-
   return (
     <>
-      <h1>HomePage</h1>
-      <Webcam />
-      <Link href="/about">Go to About</Link>
+      <h1>About</h1>
+      <Link href="/">Go to Home</Link>
     </>
   );
 };
