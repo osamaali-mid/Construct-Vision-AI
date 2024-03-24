e46f4ddd3f4d7bc518bbe28e90a26b2a769cc8ad
Amanda Natallie <amanda.natallie.2@gmail.com>
2024-03-24 17:15:50 -0300

    refactor(types): fix LoadingFullscreen type definition and return

--- src/sections/loading-fullscreen/loading-fullscreen.tsx
+++ src/sections/loading-fullscreen/loading-fullscreen.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
 import { Rings } from 'react-loader-spinner';
 
 type LoadingFullScreenProps = {
   loading: boolean;
 };
 
-function LoadingFullScreen({ loading }: LoadingFullScreenProps) {
-  return (
-    loading && (
-      <div className="z-50 absolute w-screen h-screen flex items-center justify-center bg-primary top-0 left-0">
-        Getting things ready... <Rings color="#ddd6fe" height={50} />
-      </div>
-    )
-  );
+function LoadingFullScreen({ loading }: LoadingFullScreenProps): JSX.Element | null {
+  return loading ? (
+    <div className="z-50 absolute w-screen h-screen flex items-center justify-center bg-primary top-0 left-0">
+      Getting things ready... <Rings color="#ddd6fe" height={50} />
+    </div>
+  ) : null;
 }
 
 export default LoadingFullScreen;
